9f9bbdc0a49afc900d67aa7b52575b2072153cbe
weihsin2002 <35787286+weihsin2002@users.noreply.github.com>
2018-05-20 18:31:01 -0700

    Fixed GanZhi

--- DaoCalendar/test/org/dao/calendar/test/SolarDateTest.java
+++ DaoCalendar/test/org/dao/calendar/test/SolarDateTest.java
@@ -1,16 +1,25 @@
 package org.dao.calendar.test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.apache.log4j.Logger;
+import org.dao.calendar.model.FourPillars;
 import org.dao.calendar.model.LuniSolarDate;
 import org.dao.calendar.model.SolarDate;
 
 public class SolarDateTest {
 	private final static Logger logger = Logger.getLogger(SolarDateTest.class);
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		Date date = new Date();
+		
+		String year = "2018-05-20 23 00 00";
+		
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH mm ss");  
+		date = sdf.parse(year);
+		
 		SolarDate solarDate = new SolarDate(date);
 		
 		logger.info(solarDate.toString());
@@ -20,6 +29,9 @@ public class SolarDateTest {
 		luniSolarDate = luniSolarDate.SolarToLunar(solarDate);
 		
 		logger.info(luniSolarDate.toJson().toString());
+		
+		FourPillars fourPillars = new FourPillars (solarDate, luniSolarDate);
+		logger.info(fourPillars.toJson().toString());
 	}
 
 }
